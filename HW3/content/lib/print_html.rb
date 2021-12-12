84d32c1f88c62fb77e3529e1430a1bc25d5d3dbe
Vlad Podolyan <vredbi123@gmail.com>
2021-12-12 23:05:13 +0200

    update gem

--- HW3/content/lib/print_html.rb
+++ HW3/content/lib/print_html.rb
@@ -1,5 +1,5 @@
 class PrintHtml
-  def self.print(content, bypass_html, title, file = 'index.html')
+  def self.print(content, title, file = 'index.html')
     html = <<~HTML
       <!DOCTYPE html>
       <html lang="en" dir="ltr">
